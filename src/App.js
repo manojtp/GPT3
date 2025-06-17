669f0f416095a856b69669b298e14566c5224f5c
Manoj <manojthapa016@gmail.com>
2025-06-17 18:36:57 +0100

    creating component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Article, Brand, cta, feature, navbar} from './components';
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export{ default as Article} from './article/Article';
+export{ default as Brand} from './Brand/Brand';
+export{ default as cta} from './cta/cta';
+export{ default as feature} from './feature/feature';
+export{ default as navbar} from './navbar/navbar';
